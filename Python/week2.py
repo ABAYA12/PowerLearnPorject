1f0784b1050ee08f9c9640d85bacff1140bd8d60
Ishmael Kabu Abayateye <123239306+ABAYA12@users.noreply.github.com>
2025-08-12 17:09:04 +0000

    Create week2.py
    done with week 2

---
+++ Python/week2.py
@@ -0,0 +1,29 @@
+# Create an empty list
+my_list = []
+
+# Append numbers to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+# Extend the list with more numbers
+my_list.extend([50, 60, 70])
+
+# Remove the last element
+my_list.pop()
+
+# Sort the list in ascending order
+my_list.sort()
+
+# Find the index of 30
+index_of_30 = my_list.index(30)
+
+# Print the index of 30
+print("The index of 30 is:", index_of_30)
+
+# Print the final list
+print("The final list is:", my_list)
